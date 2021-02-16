3d5dcb74be48b99ab1bdccf29f7da37de9157aba
Mohamed taha Baz <mobaz@e1r6p10.1337.ma>
2021-02-16 15:03:05 +0100

    add filter

--- Collection.php
+++ Collection.php
@@ -19,9 +19,23 @@ class Collection
         $newArr = array_map($cb, $this->collection, ...$arr);
         return new Collection($newArr);
     }
+
     public function forEach($cb) {
         foreach ($this->collection as $key => $value) {
             $cb($value, $key);
         }
     }
+
+    public function filter($cb) {
+        $newArr = [];
+        
+        $this->forEach(function ($value, $key) use (&$newArr, $cb) {
+            if ($cb($value, $key)) {
+                array_push($newArr, $value);
+            }
+        });
+
+        return new Collection($newArr);
+    }
+
 }
